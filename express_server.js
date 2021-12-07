d7cf098be1a2e875fb50b03d7c62dd415a42b4ee
Val Hustle <stars@MacBook-Air.local>
2021-12-06 22:47:56 -0500

    updated express_server.js to have post request to /urls and add short and long url to database then redirect to shorturl and get request that will redirect the request when pressed on short url to the actual page(longurl) i.e. http://youtube.com for example. Also modified the urls_new.ejs template to work with this

--- express_server.js
+++ express_server.js
@@ -3,10 +3,22 @@ const app = express();
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs"); //set view engine to EJS
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+function generateRandomString() {
+  var randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var string = '';
+  for ( var i = 0; i < 6; i++ ) { //optimized
+      string += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
+  }
+  return string;
+}
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -20,9 +32,9 @@ app.get("/urls.json", (req, res) => {
     res.json(urlDatabase);
   });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+// app.get("/hello", (req, res) => { //test hello get request
+//   res.send("<html><body>Hello <b>World</b></body></html>\n"); //res.send HTML to display on page
+// });
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
@@ -33,9 +45,28 @@ app.get("/hello", (req, res) => {
   const templateVars = { greeting: 'Hello World!' };
   res.render("hello_world", templateVars);
 });
+//get request to display/render /urls/new page
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+// get request to display /urls/shortURL page
 app.get("/urls/:shortURL", (req, res) =>{
   const shorturl = req.params.shortURL;
   const longurl = urlDatabase[shorturl];
   const templateVars = {shortURL: shorturl, longURL: longurl}
   res.render('urls_show',templateVars)
 })
+//post request to update urlDatabase and redirect us to the shorturl created
+app.post("/urls", (req, res) => {
+  const shorturl = generateRandomString();
+  const longurl = req.body.longURL;
+  urlDatabase[shorturl] = longurl;
+ 
+  console.log(req.body);  // Log the POST 
+  res.redirect(`/urls/${shorturl}`);    // Respond with 'redirect'
+});
+//get request to display on URL bar the /u/shortURL (generated) and if pressed to redirect to longurl stored (i.e. http://www.google.com)
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
